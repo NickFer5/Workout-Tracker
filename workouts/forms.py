39f07f005a18dfae1364a8e094855736ed4bd67d
NickFer5 <nicholas.ferington@me.com>
2025-06-02 12:22:08 -0400

    Delete workouts directory

--- workouts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Workout
-
-class WorkoutForm(forms.ModelForm):
-    class Meta:
-        model = Workout
-        fields = ['exercise_name', 'sets', 'reps', 'duration_minutes']
